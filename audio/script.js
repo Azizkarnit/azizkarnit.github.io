dd125813a11d2e344b8fbd4c4a208231cd9497a0
Azizkarnit <102688771+Azizkarnit@users.noreply.github.com>
2023-09-07 00:44:48 +0100

    audio update 1

--- audio/script.js
+++ audio/script.js
@@ -1,72 +1,85 @@
-// Define an array to store selected audio files
-let selectedAudioFiles = [];
+// Function to load and display audio data from localStorage
+function loadAndDisplayAudioData() {
+    const audioContainer = document.getElementById('audioContainer');
 
-// Function to load saved audio data from localStorage
-function loadAudioData() {
+    // Clear the existing audio elements
+    audioContainer.innerHTML = '';
+
+    // Load saved audio data from localStorage
     const savedData = localStorage.getItem('selectedAudioFiles');
     if (savedData) {
-        selectedAudioFiles = JSON.parse(savedData);
+        const selectedAudioFiles = JSON.parse(savedData);
+
+        // Iterate through the stored audio files and create elements
+        for (const audioFile of selectedAudioFiles) {
+            const objectURL = audioFile.objectURL;
+            const songName = audioFile.name;
+
+            // Create a container for the audio element, song name, and remove button
+            const audioItemContainer = document.createElement('div');
+
+            // Create a new audio element
+            const newAudioElement = document.createElement('audio');
+            newAudioElement.controls = true;
+            newAudioElement.src = objectURL;
+
+            // Create a paragraph for the song name
+            const songNameParagraph = document.createElement('p');
+            songNameParagraph.textContent = songName;
+
+            // Create a remove button
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.addEventListener('click', () => {
+                audioContainer.removeChild(audioItemContainer);
+                URL.revokeObjectURL(objectURL);
+                // Remove the selected file from the array
+                const indexToRemove = selectedAudioFiles.findIndex(item => item.name === songName);
+                if (indexToRemove !== -1) {
+                    selectedAudioFiles.splice(indexToRemove, 1);
+                    // Save updated audio data
+                    localStorage.setItem('selectedAudioFiles', JSON.stringify(selectedAudioFiles));
+                }
+            });
+
+            // Append the audio element, song name, and remove button to the container
+            audioItemContainer.appendChild(newAudioElement);
+            audioItemContainer.appendChild(songNameParagraph);
+            audioItemContainer.appendChild(removeButton);
+
+            // Append the new audio element container to the main container
+            audioContainer.appendChild(audioItemContainer);
+        }
     }
 }
 
-// Function to save audio data to localStorage
-function saveAudioData() {
-    localStorage.setItem('selectedAudioFiles', JSON.stringify(selectedAudioFiles));
-}
-
-// Load audio data when the page loads
-loadAudioData();
+// Load and display audio data when the page loads
+loadAndDisplayAudioData();
 
 function handleAudioFile() {
     const audioFileInput = document.getElementById('audioFile');
-    const audioContainer = document.getElementById('audioContainer');
-
     const selectedFile = audioFileInput.files[0];
 
-    // Check if the selected file has already been chosen
-    if (selectedFile && !selectedAudioFiles.includes(selectedFile.name)) {
+    if (selectedFile) {
         const objectURL = URL.createObjectURL(selectedFile);
         const songName = selectedFile.name;
 
-        // Create a container for the audio element, song name, and remove button
-        const audioItemContainer = document.createElement('div');
+        // Load existing audio data from localStorage
+        const savedData = localStorage.getItem('selectedAudioFiles');
+        const selectedAudioFiles = savedData ? JSON.parse(savedData) : [];
 
-        // Create a new audio element
-        const newAudioElement = document.createElement('audio');
-        newAudioElement.controls = true;
-        newAudioElement.src = objectURL;
-
-        // Create a paragraph for the song name
-        const songNameParagraph = document.createElement('p');
-        songNameParagraph.textContent = songName;
-
-        // Create a remove button
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.addEventListener('click', () => {
-            audioContainer.removeChild(audioItemContainer);
-            URL.revokeObjectURL(objectURL);
-            // Remove the selected file from the array
-            selectedAudioFiles.splice(selectedAudioFiles.indexOf(selectedFile.name), 1);
+        // Check if the selected file has already been chosen
+        if (!selectedAudioFiles.some(item => item.name === songName)) {
+            // Add the selected file to the array
+            selectedAudioFiles.push({ name: songName, objectURL });
             // Save updated audio data
-            saveAudioData();
-        });
-
-        // Append the audio element, song name, and remove button to the container
-        audioItemContainer.appendChild(newAudioElement);
-        audioItemContainer.appendChild(songNameParagraph);
-        audioItemContainer.appendChild(removeButton);
-
-        // Append the new audio element container to the main container
-        audioContainer.appendChild(audioItemContainer);
-
-        // Add the selected file to the array
-        selectedAudioFiles.push(selectedFile.name);
-        // Save updated audio data
-        saveAudioData();
-    } else {
-        // Display an error message or provide feedback to the user
-        alert('This audio file has already been chosen.');
+            localStorage.setItem('selectedAudioFiles', JSON.stringify(selectedAudioFiles));
+            // Display the updated audio data
+            loadAndDisplayAudioData();
+        } else {
+            // Display an error message or provide feedback to the user
+            alert('This audio file has already been chosen.');
+        }
     }
 
     // Clear the file input to allow selecting the same file again
